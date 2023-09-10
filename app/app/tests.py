1cc0824a1fa50e6df5b30968cef842eecf0cbb93
phanquoctuan20081998 <32617517+phanquoctuan20081998@users.noreply.github.com>
2023-09-10 16:45:31 +0900

    Set up database.

---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the cals module."""
+
+    def test_add_numvers(self):
+        """Test adding numbvers together."""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtractiong numbers."""
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
