0c96a18fbe22a919bafe1636da35e0154f03fed8
phanquoctuan20081998 <32617517+phanquoctuan20081998@users.noreply.github.com>
2023-09-28 23:14:57 +0900

    Add authentication api

--- app/user/urls.py
+++ app/user/urls.py
@@ -7,6 +7,8 @@ from user import views
 
 app_name = 'user'
 
-urlpatterms = [
+urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('me/', views.ManageUserView.as_view(), name='me'),
 ]
