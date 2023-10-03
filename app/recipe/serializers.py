81e7cffb013650a8954627d3d34b6787a61ab3c9
phanquoctuan20081998 <32617517+phanquoctuan20081998@users.noreply.github.com>
2023-10-03 21:47:38 +0900

    Added recipe api

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -15,7 +15,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         read_only_fields = ['id']
 
 
-class recipeDetailSerilizer(RecipeSerializer):
+class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
 
     class Meta(RecipeSerializer.Meta):
